636723e19a99e344056e24688d1e4ba2375a6a01
Miguel Callizaya <miguel.callizaya@gmail.com>
2025-02-20 02:12:43 -0400

    main modificado

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
+      <BrowserRouter basename="/tdah-pages"></BrowserRouter>
       <App />
     </StrictMode>
   );
